886f08ef12f4653c736359678be46a66b86b17a8
Yuta Okafuji <ofuji.works@gmail.com>
2023-10-05 13:01:37 +0900

    wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,5 +6,18 @@ config.font_size = 16.0
 
 config.disable_default_key_bindings = true
 
+config.keys = {
+	{
+		key = 'c',
+		mods = 'SUPER',
+		action = wezterm.action.CopyTo 'Clipboard',
+	},
+	{
+		key = 'v',
+		mods = 'SUPER',
+		action = wezterm.action.PasteFrom 'Clipboard'
+	}
+} 
+
 return config
 
