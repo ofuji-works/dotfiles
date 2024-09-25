cecdcc9d504133d121e5dc497540c53f3e651e1b
Yuta Okafuji <ofuji.works@gmail.com>
2024-09-25 10:51:44 +0900

    fix: vim error
    add vnip plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -107,6 +107,8 @@ require("lazy").setup({
 		dependencies = {
 			{ 'hrsh7th/cmp-nvim-lsp' },
 			{ "hrsh7th/cmp-buffer" },
+      { 'hrsh7th/cmp-vsnip' },
+      { 'hrsh7th/vim-vsnip' },
 			{ 'L3MON4D3/LuaSnip' },
 			{ "saadparwaiz1/cmp_luasnip" },
 		},
